a43686ebffefe4e4980ad301a153795fa9ea663c
M Rasyid Rusdiansyah <rrasyid380@gmail.com>
2025-11-17 18:46:19 +0700

    revisi tadi yang sebelum nya belum di save

--- Soal10.java
+++ Soal10.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Pertemuan6;
 
-/**
- *
- * @author Lenovo
- */
+import java.util.ArrayList;
+
 public class Soal10 {
-    
+    public static void main(String[] args) {
+        ArrayList<Object> data = new ArrayList<>();
+        
+        data.add(19);
+        data.add(0.2);
+        data.add('C');
+        data.add("Java");
+        data.add(true);
+        
+    }
 }
