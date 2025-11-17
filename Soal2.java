d8da42c2f2c553021df01c357e057b25cd42f537
M Rasyid Rusdiansyah <rrasyid380@gmail.com>
2025-11-17 18:37:02 +0700

    Add files via upload

---
+++ Soal2.java
@@ -0,0 +1,19 @@
+
+package Pertemuan6;
+
+public class Soal2 {
+    public static void main(String[] args) {
+        String[] mobil = {"Tesla","Suzuki","Civic","Mazda","BMW"};
+        for (String wadah : mobil) {
+            System.out.println(wadah);
+            
+        }
+            System.out.println("-------Setelah di ubah-------");
+            
+            mobil[1] = "Ferrary";
+            
+            for (String wadah2 : mobil) {
+                System.out.println(wadah2);
+        }
+    }
+}
---
+++ soal1.java
@@ -0,0 +1,10 @@
+
+package Pertemuan6;
+
+public class soal1 {
+    public static void main(String[] args) {
+        int angka[] = {10,20,30,40,50};  
+        int[] nilai = {10,20,30,40,50};
+        int[] data = new int[10];
+    }
+}
